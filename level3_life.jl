e1706d9213beee0453787b707f0d422c29fc42f8
JuliaChurakova <jusya05@mail.ru>
2026-03-19 20:26:23 +0300

    Завершен level3

--- level3_life.jl
+++ level3_life.jl
@@ -3,29 +3,35 @@ module GameOfLife
 using Plots
 
 mutable struct Life
-    current_frame::Matrix{Int}
-    next_frame::Matrix{Int}
+    current_frame::Matrix{Int} #текущее поколение клеток 
+    next_frame::Matrix{Int} #след. поеоление клеток 
 end
+#=
+    Любая живая ячейка с двумя или тремя живыми соседями живет в следующем поколении
+    Любая мертвая клетка с ровно тремя живыми соседями становится живой клеткой
+    Любая живая ячейка с менее чем двумя живыми соседями умирает
+    Любая живая ячейка с более чем тремя живыми соседями умирает
+    
+=#
+
 
 function step!(state::Life)
     curr = state.current_frame
     next = state.next_frame
-
-    #=
-    TODO: вместо случайного шума
-    реализовать один шаг алгоритма "Игра жизнь"
-    =#
-    for i in 1:length(curr)
-        curr[i] = rand(0:1)
+    n, m = size(curr)
+    for i in 1:n, j in 1:m
+        live = 0
+        for interior_i in -1:1, interior_j in -1:1
+            if interior_i == 0 && interior_j == 0 
+                continue
+            end
+            live += curr[mod1(i + interior_i, n), mod1(j + interior_j, m)]
+        end
+        
+        next[i,j] = (curr[i,j] == 1) ? (live in (2,3) ? 1 : 0) : (live == 3 ? 1 : 0)
     end
-
-    # Подсказка для граничных условий - тор:
-    # julia> mod1(10, 30)
-    # 10
-    # julia> mod1(31, 30)
-    # 1
-
-    return nothing
+    
+    state.current_frame, state.next_frame = state.next_frame, state.current_frame
 end
 
 function (@main)(ARGS)
@@ -34,7 +40,6 @@ function (@main)(ARGS)
     init = rand(0:1, n, m)
 
     game = Life(init, zeros(n, m))
-
     anim = @animate for time = 1:100
         step!(game)
         cr = game.current_frame
